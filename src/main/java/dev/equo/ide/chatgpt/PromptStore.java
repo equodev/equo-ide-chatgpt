9e8c8f8f11e0373744a80e6789210f593bc3b898
Ned Twigg <ned.twigg@diffplug.com>
2023-06-28 09:19:06 -0700

    Improve the PromptStore defaults.

--- src/main/java/dev/equo/ide/chatgpt/PromptStore.java
+++ src/main/java/dev/equo/ide/chatgpt/PromptStore.java
@@ -22,47 +22,91 @@
 package dev.equo.ide.chatgpt;
 
 import com.diffplug.common.base.Errors;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collection;
+import java.util.TreeMap;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
-import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.osgi.service.prefs.Preferences;
 
-public class PromptStore {
+public abstract class PromptStore {
+	public enum Type {
+		PREFACE,
+		TEMPLATE
+	}
+
 	static PromptStore get() {
-		return new PromptStore(InstanceScope.INSTANCE.getNode("dev.equo.ide.chatgpt"));
+		return null;
 	}
 
-	private final Preferences prefaces, templates;
+	abstract Sub get(Type type);
 
-	PromptStore(IEclipsePreferences preferences) {
-		this.prefaces = preferences.node("prefaces");
-		this.templates = preferences.node("templates");
+	public static class BackedByEclipse extends PromptStore {
+		private final Sub prefaces, templates;
+
+		BackedByEclipse(IEclipsePreferences preferences) {
+			prefaces = parse(preferences.node("prefaces"));
+			templates = parse(preferences.node("templates"));
+		}
+
+		private Sub parse(Preferences preferences) {
+			var sub = new Sub();
+			for (String key : Errors.rethrow().get(preferences::keys)) {
+				sub.put(key, preferences.get(key, null));
+			}
+			return sub;
+		}
+
+		@Override
+		Sub get(Type type) {
+			return type == Type.PREFACE ? prefaces : templates;
+		}
 	}
 
-	public List<String> listPrefaces() {
-		return Arrays.asList(Errors.rethrow().get(prefaces::keys));
+	public static class Defaults extends PromptStore {
+		private final Sub prefaces =
+				new Sub()
+						.put(
+								"Java terse",
+								"You are an expert Java developer. For the question below, please think carefully, work step by step, and provide a concise answer. Wherever possible, respond only in code without any explanation.")
+						.put(
+								"Java verbose",
+								"You are an expert Java developer. For the question below, please think carefully, work step by step and provide a detailed answer. Describe the reasoning for your answer.")
+						.put("(None)", "");
+
+		private final Sub templates =
+				new Sub()
+						.put("Freeform", "")
+						.put("Test JUnit 5", "Write a test for the following class using JUnit 5.")
+						.put("Test JUnit 4", "Write a test for the following class using JUnit 4.")
+						.put(
+								"Modernize",
+								"Rewrite the following class using the latest syntax constructs from Java 11. Examples to modernize:\n"
+										+ "- use `var` instead of explicit type declarations where possible\n"
+										+ "- use `List<T>` instead of `Array<T>`\n"
+										+ "- use collection literals such as `List.of()`, `Set.of()`, and `Map.of()` when appropriate")
+						.put(
+								"Describe",
+								"Describe the functionality of the following class. Call special attention to any unusual aspects of the design if they are present.");
+
+		@Override
+		Sub get(Type type) {
+			return type == Type.PREFACE ? prefaces : templates;
+		}
 	}
 
-	public List<String> listTemplates() {
-		return Arrays.asList(Errors.rethrow().get(templates::keys));
-	}
+	public static class Sub {
+		private final TreeMap<String, String> values = new TreeMap<>();
 
-	public static String TEMPLATE_FREEFORM = "Freeform";
+		public Collection<String> list() {
+			return values.keySet();
+		}
 
-	public void removePreface(String key) {
-		prefaces.remove(key);
-	}
+		public String get(String key) {
+			return values.get(key);
+		}
 
-	public void removeTemplate(String key) {
-		templates.remove(key);
-	}
-
-	public void putPreface(String key, String value) {
-		prefaces.put(key, value);
-	}
-
-	public void putTemplate(String key, String value) {
-		templates.put(key, value);
+		public Sub put(String key, String value) {
+			values.put(key, value);
+			return this;
+		}
 	}
 }
